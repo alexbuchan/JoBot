c5d19babcf3ea07aa1145dd7108b8a3775a8a51c
Brad Le <brad.le444@gmail.com>
2017-07-13 19:37:00 +0200

    Add layouts

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require("express-session");
+const expressLayouts = require('express-ejs-layouts');
 const bcrypt        = require("bcrypt");
 const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -25,6 +26,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
 
 //CREATE SESSION
 app.use(session({
@@ -128,6 +130,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressLayouts);
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
