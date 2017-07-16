37aebeefd0236db3e19da8fe9d8d669d728a5a6a
Alex Buchan <alex.buchan@telefonica.net>
2017-07-16 13:56:18 +0200

    Fix login problem. No flash

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,6 +1,9 @@
 /*jshint esversion: 6*/
+
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
+const passport = require('../helpers/passport');
 var auth    = require('../helpers/auth');
 
 /* GET home page. */
@@ -13,6 +16,24 @@ router.get('/', (req, res, next) => {
 router.get('/userProfile', auth.checkLoggedIn('You must be logged in', '/'), function(req, res, next) {
   res.render('userProfile', { user: JSON.stringify(req.user) });
 });
+
+router.post("/", passport.authenticate("local", {
+  successRedirect: "/userProfile",
+  failureRedirect: "/",
+  failureFlash: true, //disable/enable flash messaging but need flash package
+  passReqToCallback: true
+}));
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    console.log("USER AUTHENTICATED");
+    return next();
+  } else {
+    console.log("USER NOT AUTHENTICATED :(");
+    res.redirect('/');
+  }
+}
+
 
 router.get('/admin', auth.checkLoggedIn('You must be logged in', '/'), auth.checkCredentials('ADMIN'), function(req, res, next) {
 	// console.log(req.user);
